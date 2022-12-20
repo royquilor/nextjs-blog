bd89f387331327d405b084ff453565b9093f3305
royquilor <royquilor@gmail.com>
2022-12-20 21:29:20 +0000

    New local font

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css';
+import localFont from '@next/font/local'
+
+const myFont = localFont({ src: '../public/fonts/Mona-Sans.woff2' })
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
